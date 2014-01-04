f6bbaf350fca79c23bf419fa26d4270f06aeed1e
Chase Jensen <soccerchase@gmail.com>
2014-01-04 16:34:13 -0700

    Search resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140104225416) do
+ActiveRecord::Schema.define(version: 20140104233021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20140104225416) do
     t.string   "title"
     t.text     "body"
     t.datetime "published_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "searches", force: true do |t|
+    t.string   "query"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
