2e2c7b3aa09350e778367c19b3daa163c51475c5
Chase Jensen <soccerchase@gmail.com>
2014-01-05 10:23:03 -0700

    Add specs for article

---
+++ spec/factories/articles.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :article do
+    title "MyString"
+    slug "MyString"
+    body "MyText"
+  end
+end
