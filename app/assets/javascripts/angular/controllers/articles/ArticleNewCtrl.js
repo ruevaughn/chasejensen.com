c95cb6c291813ddba24af8814ef98a786a3e7f6c
Chase Jensen <soccerchase@gmail.com>
2014-01-05 13:43:29 -0700

    Tagging in Angular

--- app/assets/javascripts/angular/controllers/articles/ArticleNewCtrl.js
+++ app/assets/javascripts/angular/controllers/articles/ArticleNewCtrl.js
@@ -1,3 +1,6 @@
 myApp.controller('ArticleNewCtrl', ['$scope', function($scope) {
-
+  $scope.article = { tags: [] };
+  $scope.addTag = function(articleTag){
+    $scope.article.tags.push(articleTag)
+  }
 }]);
