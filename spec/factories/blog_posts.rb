90b95d5143b4c45a61273f73b6ddb0e8b955394b
Chase Jensen <soccerchase@gmail.com>
2014-01-05 10:20:30 -0700

    Remove BlogPost in favor of Article

--- spec/factories/blog_posts.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :blog_post do
-    title "MyString"
-    body "MyText"
-    published_at "2014-01-04 15:53:01"
-  end
-end
