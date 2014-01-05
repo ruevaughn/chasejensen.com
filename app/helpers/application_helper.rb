448e12b01de6a647055fb89408ba1c972bcd06f4
Chase Jensen <soccerchase@gmail.com>
2014-01-05 14:31:17 -0700

    Added Coderay and Redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  class MarkdownRenderer < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.highlight(code, language)
+    end
+  end
+
+  def markdown(text)
+    renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+    options = {
+    autolink: true,
+    no_intra_emphasis: true,
+    fenced_code_blocks: true,
+    lax_html_blocks: true,
+    strikethrough: true,
+    superscript: true
+    }
+    Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+  end
 end
