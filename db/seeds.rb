e34f68cf8d504b551f1796b0a91b4b264774e615
Chase Jensen <soccerchase@gmail.com>
2014-01-16 18:42:50 -0700

    Removed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'faker'
 
 Article.delete_all
-10.times do
-  Article.create!(title: Faker::Lorem.words.join(' '), body: Faker::Lorem.paragraphs.join('\n'))
-end
+# 10.times do
+#   Article.create!(title: Faker::Lorem.words.join(' '), body: Faker::Lorem.paragraphs.join('\n'))
+# end
